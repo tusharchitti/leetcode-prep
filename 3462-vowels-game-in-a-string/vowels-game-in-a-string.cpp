3e5d08a6c8e0b4cb2feeb8c1f737c0791c518bfb
tusharchitti <rainatushar221995@gmail.com>
2024-07-27 09:08:41 +0530

    Time: 37 ms (92.88%) | Memory: 17.5 MB (30.65%) - LeetSync

--- 3462-vowels-game-in-a-string/vowels-game-in-a-string.cpp
+++ 3462-vowels-game-in-a-string/vowels-game-in-a-string.cpp
@@ -1,14 +1,11 @@
 class Solution {
 public:
     bool doesAliceWin(string s) {
-        int cnt=0;
         for(int i=0;i<s.size();i++){
             if(s[i] =='a' || s[i] =='e' || s[i] =='o' || s[i] =='i' || s[i] =='u'){
-             cnt++;
-             break;
+             return true;
             }
         }
-        if(cnt == 0) return false;
-        return true;
+        return false;
     }
 };
