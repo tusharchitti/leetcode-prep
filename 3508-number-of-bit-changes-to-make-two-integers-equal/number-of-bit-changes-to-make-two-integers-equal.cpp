623c172116720fca2309910a9140d407ef5cc716
tusharchitti <rainatushar221995@gmail.com>
2024-07-27 08:54:54 +0530

    Time: 4 ms (30.38%) | Memory: 8.1 MB (33.56%) - LeetSync

--- 3508-number-of-bit-changes-to-make-two-integers-equal/number-of-bit-changes-to-make-two-integers-equal.cpp
+++ 3508-number-of-bit-changes-to-make-two-integers-equal/number-of-bit-changes-to-make-two-integers-equal.cpp
@@ -2,10 +2,11 @@ class Solution {
 public:
     int minChanges(int n, int k) {
         if(n==k) return 0;
-        if(((n^k)&k) !=0) {
+        int m = n^k;
+        if(((m)&k) !=0) {
             return -1;
         }
-        int rem = ((n^k)&n);
+        int rem = ((m)&n);
         int ans=0;
         while(rem){
            if(rem%2 == 1) ans++;
