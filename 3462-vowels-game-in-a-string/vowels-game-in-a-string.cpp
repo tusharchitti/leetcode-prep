1c0138524fa72775a484e372c8ca548f04e52532
tusharchitti <rainatushar221995@gmail.com>
2024-07-27 09:06:37 +0530

    Time: 41 ms (79.14%) | Memory: 17.4 MB (76.36%) - LeetSync

---
+++ 3462-vowels-game-in-a-string/vowels-game-in-a-string.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool doesAliceWin(string s) {
+        int cnt=0;
+        for(int i=0;i<s.size();i++){
+            if(s[i] =='a' || s[i] =='e' || s[i] =='o' || s[i] =='i' || s[i] =='u'){
+             cnt++;
+            }
+        }
+        if(cnt == 0) return false;
+        if(cnt%2 ==0) return true;
+        return true;
+    }
+};
