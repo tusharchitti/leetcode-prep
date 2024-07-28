cf284c58680d18f43b31da6cae0df2903d01d685
tusharchitti <rainatushar221995@gmail.com>
2024-07-28 13:59:16 +0530

    Time: 90 ms (53.85%) | Memory: 8.3 MB (100.00%) - LeetSync

--- 3507-find-the-count-of-numbers-which-are-not-special/find-the-count-of-numbers-which-are-not-special.cpp
+++ 3507-find-the-count-of-numbers-which-are-not-special/find-the-count-of-numbers-which-are-not-special.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     bool isPrime(int n) {
+        if(n == 1) return false;
         if(n ==1 || n== 2 || n==3||n==5) return true;
         if(n == 4) return false;
         for(int i=2;i<=sqrt(n);i++){
@@ -12,16 +13,23 @@ public:
         int strt = sqrt(l);
         int end = sqrt(r);
         int ans = 0;
-        if(strt !=1){
-        if(strt*strt == l && isPrime(strt)) ans++;
-        }
-        if(end!=1){
-        if(end*end <=r && end*end>l && isPrime(end)){ 
-            ans++;
-            } 
-        }
-        for(int i=strt+1;i<end;i++){
-            if(isPrime(i)) ans++;
+        // if(strt !=1){
+        // if(strt*strt == l && isPrime(strt)) ans++;
+        // }
+        // if(end!=1){
+        // if(end*end <=r && end*end>l && isPrime(end)){ 
+        //     ans++;
+        //     } 
+        // }
+        // for(int i=strt+1;i<end;i++){
+        //     if(isPrime(i)) ans++;
+        // }
+        if(strt*strt<l) strt++;
+        if(end*end>=l){
+            for(int i = strt;i<=end;i++){
+                //cout<<i<<" -- "<<isPrime(i)<<endl;
+                if(isPrime(i)) ans++;
+            }
         }
         return (r-l+1) - (ans);
     }
