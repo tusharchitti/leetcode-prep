254710a3b13a747c566d4cb8824c96ce147b4e8e
tusharchitti <rainatushar221995@gmail.com>
2024-12-30 19:02:20 +0530

    Time: 4 ms (53.87%) | Memory: 21.8 MB (90.54%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.cpp
+++ 36-valid-sudoku/valid-sudoku.cpp
@@ -1,30 +1,34 @@
-#include <vector>
-using namespace std;
-
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        int rows[9] = {0}, columns[9] = {0}, boxes[9] = {0};
-        
-        for (int i = 0; i < 9; ++i) {
-            for (int j = 0; j < 9; ++j) {
-                if (board[i][j] == '.') continue;
-                
-                int num = board[i][j] - '1'; // Convert '1'-'9' to 0-8
-                int mask = 1 << num;         // Create bitmask for the number
-                int boxIndex = (i / 3) * 3 + j / 3;
-                
-                // Check if the number is already set in the row, column, or box
-                if (rows[i] & mask || columns[j] & mask || boxes[boxIndex] & mask) {
-                    return false;
-                }
-                
-                // Mark the number in the row, column, and box
-                rows[i] |= mask;
-                columns[j] |= mask;
-                boxes[boxIndex] |= mask;
+     vector<long int> row(9,0);
+     vector<long int> col(9,0);
+     vector<long int> grid(9,0);
+
+     for(int i=0;i<9;i++) {
+        for(int j=0;j<9;j++) {
+            if(board[i][j]!='.'){
+            int gi = (i/3)*3+(j/3);
+            long int no = (long int)1<<(board[i][j]-'0');
+            if(row[i]&no) {
+                cout<<i<<" "<<board[i][j]<<" "<<row[i]<<" "<<no<<"\n";
+                return false;
+            }
+            if(col[j]&no) {
+                 cout<<j<<" "<<col[j]<<" "<<no<<"\n";
+                return false;
+            }
+            if(grid[gi]&no) {
+                 cout<<gi<<" "<<grid[gi]<<" "<<no<<"\n";
+                return false;
+            }
+
+            row[i]=row[i]+no;
+            col[j] = col[j]+no;
+            grid[gi] = grid[gi]+no;
             }
         }
-        return true;
+     }
+     return true;
     }
 };
