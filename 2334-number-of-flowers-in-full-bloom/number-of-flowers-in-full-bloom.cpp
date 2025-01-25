4fed1a4c2c6898a3570064b8a83bdc10bdbd5b66
tusharchitti <rainatushar221995@gmail.com>
2025-01-25 11:35:33 +0530

    Time: 754 ms (5.09%) | Memory: 86.7 MB (80.93%) - LeetSync

---
+++ 2334-number-of-flowers-in-full-bloom/number-of-flowers-in-full-bloom.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    vector<int> fullBloomFlowers(vector<vector<int>>& flowers, vector<int>& people) {
+        vector<int> start;
+        vector<int> end;
+        int flowerSize = flowers.size();
+        for(int i=0;i<flowers.size();i++)
+        {
+            start.push_back(flowers[i][0]);
+            end.push_back(flowers[i][1]+1);
+        }
+        sort(start.begin(),start.end());
+        sort(end.begin(), end.end());
+        vector<int> ans;
+        cout<<start[0]<<endl;
+        for(int i=0;i<people.size();i++)
+        {
+            int started = 0;
+            int ended = 0;
+            auto it = std::lower_bound(start.begin(), start.end(), people[i]+1);
+            if(it == start.end())
+            {
+                 started = flowerSize;
+            }
+            else
+            {
+                int distance = std::distance(start.begin(), it);
+                started = distance ;
+            }
+
+            auto it1 = std::lower_bound(end.begin(), end.end(), people[i]+1);
+            if(it1 == end.end())
+            {
+                 ended = flowerSize;
+            }
+            else
+            {
+                int distance = std::distance(end.begin(), it1);
+                ended = distance ;
+            }
+            cout<<started<<" "<<ended<<endl;
+            ans.push_back(started-ended);
+        }
+        return ans;
+    }
+};
