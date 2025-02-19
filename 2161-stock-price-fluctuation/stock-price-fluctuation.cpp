791d3c51f6116b8fae33d6de6ee7c77071e1aaef
tusharchitti <rainatushar221995@gmail.com>
2025-02-19 08:50:50 +0530

    Time: 167 ms (13.95%) | Memory: 175.6 MB (20.93%) - LeetSync

---
+++ 2161-stock-price-fluctuation/stock-price-fluctuation.cpp
@@ -0,0 +1,45 @@
+class StockPrice {
+public:
+    multiset<int>stockPriceSet;
+    map<int,int> stockPriceMap;
+    int maxTimestamp;
+
+    StockPrice() {
+        maxTimestamp = 0;
+    }
+    
+    void update(int timestamp, int price) {
+        if(timestamp > maxTimestamp) 
+        {
+            maxTimestamp = timestamp;
+        }
+
+        if(stockPriceMap.find(timestamp) != stockPriceMap.end())
+        {
+            stockPriceSet.erase(stockPriceSet.find(stockPriceMap[timestamp]));
+        }
+        stockPriceSet.insert(price);
+        stockPriceMap[timestamp] = price;
+    }
+    
+    int current() {
+        return stockPriceMap[maxTimestamp];
+    }
+    
+    int maximum() {
+        return *stockPriceSet.rbegin();
+    }
+    
+    int minimum() {
+        return *stockPriceSet.begin();
+    }
+};
+
+/**
+ * Your StockPrice object will be instantiated and called as such:
+ * StockPrice* obj = new StockPrice();
+ * obj->update(timestamp,price);
+ * int param_2 = obj->current();
+ * int param_3 = obj->maximum();
+ * int param_4 = obj->minimum();
+ */
