81639d9ccc50c1544f79c888cf7871620c6aa4b2
tusharchitti <rainatushar221995@gmail.com>
2025-02-19 10:58:19 +0530

    Time: 23 ms (8.99%) | Memory: 29.3 MB (5.05%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -2,10 +2,6 @@ class Solution {
 public:
     vector<int> MergeOverlappingInterval(vector<int> first, vector<int> second)
     {
-          // case 1 [2, 4] [5,6] => no change-done
-          // case 2  [2,4] [2 8] => merge them done
-          // case 3   [2,5] [3,5] =>  merge them
-          // case 4   [2,5] [3,4] => merge them
           if(second[0] <= first[1])
           {
             return {first[0],max(first[1],second[1])};
@@ -20,14 +16,15 @@ public:
         for(int i=1;i<intervals.size();i++)
         {
            vector<int> recentMergedVector = mergedVector.back();
-           if(MergeOverlappingInterval(recentMergedVector, intervals[i]).size() == 0)
+           vector<int> mergeVector =  MergeOverlappingInterval(recentMergedVector, intervals[i]);
+           if(mergeVector.size() == 0)
            {
             mergedVector.push_back(intervals[i]);
            }
            else
            {
             mergedVector.pop_back();
-            mergedVector.push_back(MergeOverlappingInterval(recentMergedVector, intervals[i]));
+            mergedVector.push_back(mergeVector);
            }
         }
         return mergedVector;
