44ea84242ab7277396a24e839eaad0b137c425a5
tusharchitti <rainatushar221995@gmail.com>
2025-02-25 20:36:30 +0530

    Time: 7 ms (41.25%) | Memory: 12.2 MB (16.19%) - LeetSync

--- 1186-building-h2o/building-h2o.cpp
+++ 1186-building-h2o/building-h2o.cpp
@@ -1,59 +1,48 @@
-#include <iostream>
-#include <functional>
-#include <mutex>
-#include <condition_variable>
-
-using namespace std;
-
 class H2O {
-private:
-    int hydrogenCount = 0;  // Track hydrogen threads
-    int oxygenCount = 0;    // Track oxygen threads
+public:
+    int HCount =0;
+    int OCount =0;
     mutex mtx;
     condition_variable cv;
-
-public:
-    H2O() {}
+    H2O() {
+        
+    }
 
     void hydrogen(function<void()> releaseHydrogen) {
-        unique_lock<mutex> lock(mtx);
-        
-        // Wait until there are fewer than 2 hydrogen threads
-        while (hydrogenCount >= 2) {
-            cv.wait(lock);
+        unique_lock<mutex> lock1(mtx);
+
+        while(HCount>=2)
+        {
+            cv.wait(lock1);
         }
 
-        hydrogenCount++;
-
-        // Release Hydrogen molecule
+        HCount++;
+        // releaseHydrogen() outputs "H". Do not change or remove this line.
         releaseHydrogen();
 
-        // If we have 2 H and 1 O, signal threads to proceed
-        if (hydrogenCount == 2 && oxygenCount == 1) {
-            hydrogenCount = 0;
-            oxygenCount = 0;
-            cv.notify_all();  // Allow the next set of threads
+        if(HCount ==2 && OCount ==1)
+        {
+            HCount = 0;
+            OCount = 0;
+            cv.notify_all();
         }
     }
 
     void oxygen(function<void()> releaseOxygen) {
-        unique_lock<mutex> lock(mtx);
-
-        // Wait until there is less than 1 oxygen thread
-        while (oxygenCount >= 1) {
-            cv.wait(lock);
+        unique_lock<mutex> lock2(mtx);
+        // releaseOxygen() outputs "O". Do not change or remove this line.
+        while(OCount>=1)
+        {
+            cv.wait(lock2);
         }
+        OCount++;
 
-        oxygenCount++;
-
-        // Release Oxygen molecule
         releaseOxygen();
-
-        // If we have 2 H and 1 O, signal threads to proceed
-        if (hydrogenCount == 2 && oxygenCount == 1) {
-            hydrogenCount = 0;
-            oxygenCount = 0;
-            cv.notify_all();  // Allow the next set of threads
+        if(HCount ==2 && OCount ==1)
+        {
+            HCount = 0;
+            OCount = 0;
+            cv.notify_all();
         }
     }
 };
